652a4c22de3d25dbb469b5c71b2422a405cbc733
SarahJ06 <107339722+SarahJ06@users.noreply.github.com>
2022-06-16 19:16:25 +1000

    Add files via upload

---
+++ portfolio/myapp/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def hello(request):
+    return render(request, "myapp/base.html")
+
+def portfolio(request):
+    return HttpResponse("Welcome to my portfolio")
